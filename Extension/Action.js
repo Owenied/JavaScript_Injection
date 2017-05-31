0162a9ee0fd1aca3863f1d3e809d812e1d1d4d3d
Owen Duignan <owenied@me.com>
2017-05-31 12:31:30 -0500

    Modified the extension plist file for extension to work and added a basic javascript file

---
+++ Extension/Action.js
@@ -0,0 +1,15 @@
+var Action = function() {};
+
+Action.prototype = {
+    
+run: function(parameters) {
+    
+},
+
+    finalise: function(parameters) {
+        
+}
+
+};
+
+var ExtensionPreprocessingJS = new Action
